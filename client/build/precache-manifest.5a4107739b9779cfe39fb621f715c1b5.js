f34292d07af6dd84458fa853d7ccf398f30d31e7
HelenaMadrid <helena.madrid@directrips.com>
2019-08-11 13:39:48 -0500

    j

---
+++ client/build/precache-manifest.5a4107739b9779cfe39fb621f715c1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a684742c1b575f3cd9f",
+    "url": "/static/js/main.bf6b3c24.chunk.js"
+  },
+  {
+    "revision": "867c3426fce5d60dfaf9",
+    "url": "/static/js/2.795f3468.chunk.js"
+  },
+  {
+    "revision": "6a684742c1b575f3cd9f",
+    "url": "/static/css/main.11baf1bc.chunk.css"
+  },
+  {
+    "revision": "4788635b63c21f6b2c258b39f1768147",
+    "url": "/index.html"
+  }
+];
